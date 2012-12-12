d559ac3cbe181856716ac9a7be326889a5d330c5
Mike Splain <mike.splain@gmail.com>
2012-12-11 21:42:33 -0500

    Allow testing on production servers (QA). Never for REAL production.

--- lib/better_errors/rails.rb
+++ lib/better_errors/rails.rb
@@ -1,12 +1,10 @@
 module BetterErrors
   class Railtie < Rails::Railtie
     initializer "better_errors.configure_rails_initialization" do
-      unless Rails.env.production?
-        Rails.application.middleware.use BetterErrors::Middleware
-        Rails.application.middleware.insert_before Rails::Rack::Logger, BetterErrors::DisableLoggingMiddleware
-        BetterErrors.logger = Rails.logger
-        BetterErrors.application_root = Rails.root.to_s
-      end
+      Rails.application.middleware.use BetterErrors::Middleware
+      Rails.application.middleware.insert_before Rails::Rack::Logger, BetterErrors::DisableLoggingMiddleware
+      BetterErrors.logger = Rails.logger
+      BetterErrors.application_root = Rails.root.to_s      
     end
   end
 end
